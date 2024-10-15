440c755599e8fd88306671969875c1cf8e7e21ea
SGuevaraClark <sebastianguevarac@gmail.com>
2024-10-15 15:34:06 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const card = document.querySelector("#card"); 
+const paloUp = document.querySelector('#paloUp');
+const paloDown = document.querySelector('#paloDown');
+const number = document.querySelector('#number');
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const cardNumbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+const palos = ['♦', '♥', '♠', '♣'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const getRandomItem  = (arr) => {
+    return arr[(Math.floor(Math.random()* arr.length))];
 };
+
+function randomCard() {
+    const randomPalo = getRandomItem(palos);
+    const randomNumber = getRandomItem(cardNumbers);
+
+    paloUp.textContent = randomPalo; // textcontent pareciera mejor que innerHTML
+    paloDown.textContent = randomPalo;
+    number.textContent = randomNumber;
+
+    if (randomPalo === '♥' || randomPalo === '♦') {
+        paloUp.style.color = 'red';
+        paloDown.style.color = 'red';
+        number.style.color = 'red';
+    }
+}
+
+window.onload = randomCard;
